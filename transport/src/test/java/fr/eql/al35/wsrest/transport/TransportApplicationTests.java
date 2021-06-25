309af74fd6f39d496298ed9a85579f7bd2316591
Floriane <floriane.ahadi@gmail.com>
2021-06-25 11:16:58 +0200

    2eme push ws transport

---
+++ transport/src/test/java/fr/eql/al35/wsrest/transport/TransportApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eql.al35.wsrest.transport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
