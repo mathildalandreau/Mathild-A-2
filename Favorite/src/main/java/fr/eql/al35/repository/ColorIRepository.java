f4feb560f14e96c0191d3e3ae1898a74b3bd9172
mathildalandreau <73292823+mathildalandreau@users.noreply.github.com>
2021-06-25 16:26:40 +0200

    Delete Favorite directory

--- Favorite/src/main/java/fr/eql/al35/repository/ColorIRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.eql.al35.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import fr.eql.al35.entity.Color;
-
-public interface ColorIRepository extends CrudRepository<Color, Integer> {
-
-}
--- Favorite/src/main/java/fr/eql/al35/repository/LocationIRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.eql.al35.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import fr.eql.al35.entity.Location;
-
-public interface LocationIRepository extends CrudRepository<Location, String>{
-
-}
