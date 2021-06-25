9891ab1cae3e758b4ae9be3bdeabf9bc0731d9e0
stephane <stephane.raes@gmail.com>
2021-06-25 14:56:59 +0200

    commit projet packaging

---
+++ packaging/src/main/java/fr/eql/al35/wsrest/packaging/PackagingApplication.java
@@ -0,0 +1,13 @@
+package fr.eql.al35.wsrest.packaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagingApplication.class, args);
+	}
+
+}
---
+++ packaging/src/test/java/fr/eql/al35/wsrest/packaging/PackagingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eql.al35.wsrest.packaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
