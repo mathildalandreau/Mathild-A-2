fc3f14d3c4c5666df24477e589c4090c76efe84f
stephane <stephane.raes@gmail.com>
2021-06-30 09:07:29 +0200

    + Tests unitaires transport

--- transport/src/test/java/fr/eql/al35/wsrest/testsTransport/GetTransporter.java
+++ transport/src/test/java/fr/eql/al35/wsrest/testsTransport/GetTransporter.java
@@ -2,6 +2,8 @@ package fr.eql.al35.wsrest.testsTransport;
 
 import java.util.List;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,29 +14,83 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import fr.eql.al35.wsrest.transport.TransportApplication;
 import fr.eql.al35.wsrest.transport.entity.Tarif;
+import fr.eql.al35.wsrest.transport.entity.Transporteur;
 import fr.eql.al35.wsrest.transport.service.TransportService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {TransportApplication.class})
 class GetTransporter {
-	
+
 	@Autowired
 	private TransportService transportService; 
-	
-	@Test
-	@DisplayName("TU Calcul fees for an initial weight of 0.4 kg")
-	void TU_CalculFees_04() {
-		List<Tarif> list = transportService.calculateTarifs(0.4);
-		assertEquals("La Poste", list.get(0).getTransporteur().getName()); 
-		}
 
-		
+
+	@Test
+	@DisplayName("Transporter name La Poste should exists")	
+	void TU_CalculFees_LaPoste() {
+		List<Tarif> list = transportService.calculateTarifs(0.4); 
+		for (int i = 0; i < list.size(); i++) {			
+			assertEquals("La Poste", list.get(0).getTransporteur().getName(), "incorrect transporter Name");
+		}
 	}
+
+	@Test
+	@DisplayName("Transporter name Mondial Relay should exists")
+	void TU_CalculFees_MondialRelay() {
+		List<Tarif> list = transportService.calculateTarifs(0.4); 
+		for (int i = 0; i < list.size(); i++) {			
+			assertEquals("Mondial Relay", list.get(1).getTransporteur().getName(), "incorrect transporter Name");
+		}
+	}
+
+	@Test
+	@DisplayName("Transporter name UPS should exists ")
+	void TU_CalculFees_UPS() {
+		List<Tarif> list = transportService.calculateTarifs(0.4); 
+		for (int i = 0; i < list.size(); i++) {		
+			assertEquals("UPS", list.get(2).getTransporteur().getName(), "incorrect transporter Name");
+		}
+	}
+
+	@Test
+	@DisplayName("Transporter name should only be one of the list")
+	void TU_CalculFees_Incorrect_transporter() {
+		List<Tarif> list = transportService.calculateTarifs(0.4); 
+		for (int i = 0; i < list.size(); i++) {		
+			assertNotEquals("UP", list.get(2).getTransporteur().getName());			
+		}
+	}
+
+	@Test
+	@DisplayName("Transporter ID")
+	void TU_CalculFees_ID() {
+		List<Tarif> list = transportService.calculateTarifs(0.4); 
+		for (int i = 0; i < list.size(); i++) {		
+			assertEquals(3, list.get(2).getTransporteur().getId());
+		}
+	}
+	
+	@DisplayName("Transporter Array")
+	void TU_CalculFees_Array() {
+		List<Tarif> list = transportService.calculateTarifs(0.4); 
+		
+		for (int i = 0; i < list.size(); i++) {	
+			assertThat(list.get(i).getTransporteur().getName().equals("prout"));
+		}
+				
+		}
+		
+	
+}
 
